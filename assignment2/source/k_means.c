36d09fab70b498078e3aa480acf9b66e6df68e51
Bryan LeVau <bryvo01@gmail.com>
2024-01-30 14:20:16 -0600

    adds basic framework

--- assignment2/source/k_means.c
+++ assignment2/source/k_means.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <float.h>
 #include <math.h>
+#include <stdlib.h>
 
 #include "k_means.h"
 
@@ -33,12 +34,19 @@ void k_means(struct point p[MAX_POINTS],
 	    struct point u[MAX_CENTERS],
 	    int c[MAX_POINTS])
 {
-	int j; /* To Students: add your local variables */
+	int j;
+	int a;
+	int b;
+	int count;
+	float x_sum = 0;
+	float y_sum = 0;
+	float closest[m];
+	float distance;
+	/* To Students: add your local variables */
 
 	/* randomly initialized the centers */
 	for(j = 0; j < k; j++)
 		u[j] = random_center(); /* DO NOT change this random generator! */
-	
 	/* 
 	 * To students: please implment K-Means algorithm here 
 	 * Your K-means implementation should do "iters" rounds of clustering. After 
@@ -46,7 +54,59 @@ void k_means(struct point p[MAX_POINTS],
 	 * of your centers, and array c[MAX_POINTS] should have the cluster assignment
 	 * for each point.
 	 */
+	//float min = fminf(a, fminf(b, c));
+	//float max = fmaxf(a, fmaxf(b, c));
 	
-	
+	//initialze min[] to infinity
+	for(a = 0; a < m; a++) {
+		closest[a] = INFINITY;
+	}
+	for(b = 0; b < iters; b++) {
+		for(a = 0; a < m; a++) {
+			for(j = 0; j < k; j++) {
+				distance = pow(u[j].x - p[a].x, 2) + pow(u[j].y - p[a].y, 2);
+				if(distance < closest[a]) {
+					c[a] = j;
+					closest[a] = distance;
+				}
+			}
+		}
+		for(j = 0; j < k; j++) {
+			count = 0;
+			x_sum = 0;
+			y_sum = 0;
+			for(a = 0; a < m; a++) {
+				if(c[a] == j) {
+					count++;
+					x_sum += p[a].x;
+					y_sum += p[a].y;
+				}
+			}
+			u[j].x = x_sum/count;
+			u[j].y = y_sum/count;
+		}
+	}
+	//iterate through each point
+	/*for(j = 0; j < k; j++) {
+		//calculate the difference between a point and the center(s)
+		for(a = 0; a < m; a++) {
+			//printf("%d   xdiff: %.3lf\n", j, x_diff);
+			x_diff = pow(u[j].x - p[a].x, 2) + pow(u[j].y - p[a].y, 2);
+			y_diff = pow(u[j].y - p[a].y, 2);
+			printf("xdiff: %.3lf     min[%d]: %.3lf\n", x_diff, a, min[a]);
+			if (x_diff < min[a]) {
+				min[a] = x_diff;
+				point_cluster[a] = j;
+				printf("changed min[%d] to: %.3lf    min[%d]: %.3lf\n",a, x_diff, a, min[a]);
+			}
+			//printf("x_diff: %.3lf\n", x_diff);
+		}
+	}
+	for (a = 0; a < m; a++) {
+		printf("%d   ", point_cluster[a]);
+	}
+	//printf("number of points: %d\n", m);
+	//printf("points: %.3lf, %.3lf\n", p[0].x, p[0].y);	
+	*/
 	return;
 }
