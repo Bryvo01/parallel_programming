482c808e77735184898bf46374e3548d8c962ba4
Bryan LeVau <bryvo01@gmail.com>
2024-01-30 18:44:09 -0600

    finishes the algorithm

--- assignment2/source/k_means.c
+++ assignment2/source/k_means.c
@@ -28,85 +28,69 @@
  *     int c[]         : cluster id for each data points
  */
 void k_means(struct point p[MAX_POINTS], 
-	    int m, 
-	    int k,
-	    int iters,
-	    struct point u[MAX_CENTERS],
-	    int c[MAX_POINTS])
+	    	 int m, 
+	    	 int k,
+	    	 int iters,
+	    	 struct point u[MAX_CENTERS],
+	    	 int c[MAX_POINTS])
 {
-	int j;
-	int a;
-	int b;
-	int count;
-	float x_sum = 0;
-	float y_sum = 0;
-	float closest[m];
-	float distance;
-	/* To Students: add your local variables */
+  int j;
+  int a;
+  int b;
+  int count;
+  double x_sum = 0;
+  double y_sum = 0;
+  double min_dist;
+  double distance;
+  /* To Students: add your local variables */
 
-	/* randomly initialized the centers */
-	for(j = 0; j < k; j++)
-		u[j] = random_center(); /* DO NOT change this random generator! */
-	/* 
-	 * To students: please implment K-Means algorithm here 
-	 * Your K-means implementation should do "iters" rounds of clustering. After 
-	 * all iterations finish, array u[MAX_CENTERS] should have the coordinations 
-	 * of your centers, and array c[MAX_POINTS] should have the cluster assignment
-	 * for each point.
-	 */
-	//float min = fminf(a, fminf(b, c));
-	//float max = fmaxf(a, fmaxf(b, c));
-	
-	//initialze min[] to infinity
+  /* randomly initialized the centers */
+  for(j = 0; j < k; j++)
+	u[j] = random_center(); /* DO NOT change this random generator! */
+  /* 
+   * To students: please implment K-Means algorithm here 
+   * Your K-means implementation should do "iters" rounds of clustering. After 
+   * all iterations finish, array u[MAX_CENTERS] should have the coordinations 
+   * of your centers, and array c[MAX_POINTS] should have the cluster assignment
+   * for each point.
+   */
+  // do the iterations
+  for(b = 0; b < iters; b++) {
+	// loop through the points
 	for(a = 0; a < m; a++) {
-		closest[a] = INFINITY;
-	}
-	for(b = 0; b < iters; b++) {
-		for(a = 0; a < m; a++) {
-			for(j = 0; j < k; j++) {
-				distance = pow(u[j].x - p[a].x, 2) + pow(u[j].y - p[a].y, 2);
-				if(distance < closest[a]) {
-					c[a] = j;
-					closest[a] = distance;
-				}
-			}
+	  min_dist = INFINITY;
+	  // loop through the centers
+	  for(j = 0; j < k; j++) {
+		// get the distance
+		distance = pow((p[a].x - u[j].x), 2) + pow((p[a].y - u[j].y), 2);
+		// check if smaller than the prev
+		if(distance < min_dist) {
+		  c[a] = j;
+		  min_dist = distance;
 		}
-		for(j = 0; j < k; j++) {
-			count = 0;
-			x_sum = 0;
-			y_sum = 0;
-			for(a = 0; a < m; a++) {
-				if(c[a] == j) {
-					count++;
-					x_sum += p[a].x;
-					y_sum += p[a].y;
-				}
-			}
-			u[j].x = x_sum/count;
-			u[j].y = y_sum/count;
+	  }
+	}
+	for(j = 0; j < k; j++) {
+	  // get the sums
+	  count = 0;
+	  x_sum = 0;
+	  y_sum = 0;
+	  for(a = 0; a < m; a++) {
+		if(c[a] == j) {
+		  count++;
+		  x_sum += p[a].x;
+		  y_sum += p[a].y;
 		}
+	  }
+	  if(count > 0) {
+	    // get the new center
+	    u[j].x = x_sum/count;
+	    u[j].y = y_sum/count;
+	  } else {
+		// reset the lonely boi
+		u[j] = random_center();
+	  }
 	}
-	//iterate through each point
-	/*for(j = 0; j < k; j++) {
-		//calculate the difference between a point and the center(s)
-		for(a = 0; a < m; a++) {
-			//printf("%d   xdiff: %.3lf\n", j, x_diff);
-			x_diff = pow(u[j].x - p[a].x, 2) + pow(u[j].y - p[a].y, 2);
-			y_diff = pow(u[j].y - p[a].y, 2);
-			printf("xdiff: %.3lf     min[%d]: %.3lf\n", x_diff, a, min[a]);
-			if (x_diff < min[a]) {
-				min[a] = x_diff;
-				point_cluster[a] = j;
-				printf("changed min[%d] to: %.3lf    min[%d]: %.3lf\n",a, x_diff, a, min[a]);
-			}
-			//printf("x_diff: %.3lf\n", x_diff);
-		}
-	}
-	for (a = 0; a < m; a++) {
-		printf("%d   ", point_cluster[a]);
-	}
-	//printf("number of points: %d\n", m);
-	//printf("points: %.3lf, %.3lf\n", p[0].x, p[0].y);	
-	*/
-	return;
+}
+  return;
 }
